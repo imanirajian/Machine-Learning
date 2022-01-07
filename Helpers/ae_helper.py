ab9ed5aa2535235e633919188ab02c4441b74e07
Iman Irajian <iman.irajian@gmail.com>
2022-01-07 04:03:24 +0330

    Create ae_helper.py

---
+++ Helpers/ae_helper.py
@@ -0,0 +1,81 @@
+ae_helper.py
+Who has access
+Not shared
+System properties
+Type
+Text
+Size
+2 KB
+Storage used
+2 KB
+Location
+Helpers
+Owner
+me
+Modified
+6:34 PM by me
+Opened
+3:27 AM by me
+Created
+9:25 PM with Google Drive Web
+Add a description
+Viewers can download
+from typing import Tuple
+
+from sklearn.datasets import fetch_openml
+import numpy as np
+import os
+import torch
+from tqdm import tqdm
+
+
+def get_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """ Downloads the MNIST data and processes the data for autoencoder problem.
+    
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]: A tuple of input images,
+            output images (input + 1), and the label of input images
+    """
+    X, y = fetch_openml('mnist_784', return_X_y=True, cache=True)
+    X, y = X.values, y.values
+    X = X.reshape(-1, 28, 28).astype('float32') / 255.0
+    y = y.astype(int)
+
+    Y = np.zeros_like(X)
+    ind = y.argsort()
+    X, y = X[ind], y[ind]
+
+    c = 0
+    for i in range(10):
+        s = (y == i).sum()
+        ss = (y == (i + 1) % 10).sum()
+        if s <= ss:
+            Y[c:c+s] = X[c+s:c+2*s]
+        elif i < 9:
+            Y[c:c+ss] = X[c+s:c+s+ss]
+            Y[c+ss:c+s] = X[c+s:c+2*s-ss]
+        else:
+            Y[c:c+ss] = X[:ss]
+            Y[c+ss:c+s] = X[:s-ss]
+        c += s
+
+    current_state = np.random.get_state()
+
+    np.random.seed(10)
+    ind = np.random.permutation(X.shape[0])
+    X, Y, y = X[ind], Y[ind], y[ind]
+
+    np.random.set_state(current_state)
+
+    return X, Y, y
+
+def init_mnist_subset_directories(mnist_dataset, dataset_path, subset_size=5000):
+    mnist_subset = torch.utils.data.Subset(mnist_dataset, torch.randperm(len(mnist_dataset))[:subset_size])
+
+    os.mkdir(dataset_path)
+  
+    for idx, (x, y) in enumerate(tqdm(mnist_subset)):
+        label_path = os.path.join(dataset_path, str(y))
+        if not os.path.exists(label_path):
+            os.mkdir(label_path)
+        torch.save(x, os.path.join(label_path, 'data_'+str(idx)+'.pth'))
